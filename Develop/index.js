2c2fb65a095ade5207a8c45c6558829de971bde4
Kenny Wai <97656734+KennyWCoder@users.noreply.github.com>
2022-10-17 23:28:01 -0700

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,97 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+
+const inquirer = require('inquirer');
+
+const generateNotes = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a brief description of you project: ",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please provide an installation if appear: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What will the project use for?",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please choose a license on the list below: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "None"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of the project?",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included ",
+        },
+        {
+            type: "input",
+            name: "question",
+            message: "Question that I faced during the project: ",
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: ",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address: ",
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = data => {
+    fs.writeFile("README.md", data, err => {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("README has been successfully created!")
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions()
+        .then((answers) => {
+            return generateNotes(answers);
+        })
+        .then(data => {
+            return writeToFile(data);
+        })
+        .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+// function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => {
+    return `![${license}](https://img.shields.io/badge/License-${license}-yellow.svg)`
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+// function renderLicenseLink(license) {}
+const renderLicenseLink = (license) => {
+  if (license !== "None") {
+    return "[License](#license)";
+  } return "";
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+// function renderLicenseSection(license) {}
+const renderLicenseSection = (license) => {
+  if (license !== `None`){
+  return (
+  `## License
+  This project is license under the ${license} lincense`
+  )}
+  return ``
+}
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = data => {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - ${renderLicenseLink(data.license)}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  ${data.contributing}
 
+  ## Tests
+  ${data.tests}
+  
+  My Github: ${data.username}
+  My Email: ${data.email}
+  
 `;
 }
 
